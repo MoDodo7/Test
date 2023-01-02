9f8cdc1d6d2deb8052eff25ef9bb4e5c715671e2
MoDodo7 <mmmatouk96@gmail.com>
2023-01-02 12:27:46 +0200

    update repo

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class LinkedList
+    {
+        internal Node head;
+        public Node getLastNodeOfList()
+        {
+            Node L = head;
+            while (L.next != null)
+            {
+                L = L.next;
+            }
+          
+            return L;
+        }
+        internal void insert(int data)
+        {
+            if (head == null)
+            {
+                Node n = new Node(data);
+                n.next = null;
+                head = n;
+            }
+            else
+            {
+                Node L = getLastNodeOfList();
+                L.next = new Node(data);
+            }
+
+        }
+
+        internal void deleteNode(int key)
+        {
+
+            Node prev = head;
+            Node Current = head.next;
+
+            while (Current != null && Current.data != key)
+            {
+                prev = Current;
+                Current = Current.next;
+
+            }
+            if (Current.data == key)
+            {
+                prev.next = Current.next;
+
+            }
+
+        }
+
+        internal bool deleteHead(int key)
+        {
+            if (head is null || head.data != key)
+            {
+                return false;
+            }
+
+            head = head.next;
+            return true;
+
+        }
+
+        internal bool delete(int key)
+        {
+            Node temp = head;
+            while (temp.next != null && temp.data == key)
+            {
+                temp = temp.next;
+            }
+            temp = temp.next.next;
+            return true;
+        }
+
+        internal void printList()
+        {
+
+            Node n = head;
+            while (n != null) 
+            { 
+
+
+                    Console.Write($"{n.data} -> ");
+                    n = n.next;
+                
+                
+            }
+            Console.WriteLine("null");
+
+
+
+
+        }
+
+
+    }
+    
+
+}
+
+
+
+
---
+++ LinkedList/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList L = new LinkedList();
+          for(int i = 0; i < 11; i++)
+            {
+                L.insert(i);
+            }
+           
+            L.printList();
+            L.delete(5);
+            Console.WriteLine("//////////////////////////////////");
+            L.printList();
+            Console.ReadLine();
+        }
+    }
+}
